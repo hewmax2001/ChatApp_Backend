5649bf604794669598850375928141b1d62760c1
Max Hewitt <mxhew20@gmail.com>
2024-04-26 14:03:44 +1200

    Implement failed session authentication catch

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,11 +1,15 @@
 // Errors that occur in async functions resolve to here
 const errorHandler = (error, request, response, next) => {
+    console.log('This is the error:', error.name)
     switch (error.name) {
         case 'JsonWebTokenError':
             response.status(401).json({ error: 'token missing or invalid' })
             break
         case 'TokenExpireError':
             response.status(401).json({ error: 'token expired' })
+            break
+        case 'Failed Auth':
+            response.status(401).json({ error: error.message })
             break
     }
 
