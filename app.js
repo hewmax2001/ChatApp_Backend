896fd4fc63c669c9a6ccd05173a42f5c7770e379
Max Hewitt <mxhew20@gmail.com>
2024-04-26 14:04:19 +1200

    Implement session authentication into application
    Install additional depedencies

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
 const mongoose = require('mongoose')
 const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
+const sessionRegenerate = require('./middleware/sessionRegenerate')
 
 const config = require('./utils/config')
 
@@ -12,11 +16,10 @@ require('express-async-errors')
 // Routers
 const authRouter = require('./routes/authRouter')
 const userRouter = require('./routes/userRouter')
-const commentRouter = require('./routes/commentRouter')
+const messageRouter = require('./routes/messageRouter')
 
 // Middleware
 const middleware = require('./middleware')
-
 
 // Express app
 const app = express()
@@ -33,22 +36,40 @@ mongoose.connect(config.MONGODB_URI)
     })
 
 // Prefix middleware
-app.use(cors())
+app.use(session({
+    secret: 'some secret',
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+        mongoUrl: config.MONGODB_URI
+    }),
+    cookie: { 
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24 // 1 Day
+    }
+}))
+
+app.use(sessionRegenerate)
+app.use(cors(corsOptions))
+
 app.use(express.json())
-app.use(cookieParser())
+
 // Write this to some log file
 app.use(morgan(':method :url :status :res[conent-length] - :response-time ms'))
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
+app.options('*', cors())
 // Routes
 app.use('/api/auth', authRouter)
 app.use('/api/users', userRouter)
-app.use('/api/comment', commentRouter)
+app.use('/api/comment', messageRouter)
+app.get('/', (req, res, next) => {
+    res.send('<h1>Hello World SESSIONS</h1>')
+})
 
 // Suffix middleware
 app.use(middleware.errorHandler)
+app.use((request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+})
 
 module.exports = app
