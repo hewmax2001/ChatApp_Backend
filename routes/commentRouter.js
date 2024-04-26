c63d4ff0f92d57a95e008958509faf745ef7432b
Max Hewitt <mxhew20@gmail.com>
2024-04-26 13:56:38 +1200

    Rename comment to message

--- routes/commentRouter.js
+++
@@ -1,8 +0,0 @@
-const commentController = require('../controllers/comments')
-const commentRouter = require('express').Router()
-const userAuth = require('../middleware/tokenAuthenticator')
-
-commentRouter.post('/', userAuth, commentController.create)
-commentRouter.get('/', commentController.getAll)
-
-module.exports = commentRouter
